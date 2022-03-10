1f0b6a807786feed5018cb31d950c1cb9342c8ec
jabbarDevOps <jabbar.devops@gmail.com>
2022-03-10 15:48:06 +0000

    ajout des button edit delte update dans le module entreprise

--- view/entreprise/list.php
+++ view/entreprise/list.php
@@ -2,8 +2,6 @@
 require_once "./controller/EntrepriseController.php";
 $data = new EntrepriseController();
 $data = $data->allReponse();
-
-
 ?>
 
 
@@ -35,9 +33,9 @@ $data = $data->allReponse();
                         <td><?= $key['nomCompletRepondant'] ?></td>
                         <td><?= $key['fonctionRepondant'] ?></td>
                         <td><?= $key['telephoneRepondant'] ?></td>
-                        <td class="text-center"><a href="?route&module=delete&i="><i class="bi bi-trash3" style="color: #ce0033; font-size:1.5rem;"></i></a></td>
-                        <td class="text-center"><a href="?route&module=edit&i="><i class="bi bi-pencil-square" style="color: primary; font-size:1.5rem;"></i></a></td>
-                        <td class="text-center"><a href="?route&module=delete&i="><i class="bi bi-trash3" style="color: #ce0033; font-size:1.5rem;"></i></a></td>
+                        <td class="text-center"><a href="?route&module=delete&idEntreprise=<?= $key['idEntreprise']; ?>"><i class="bi bi-eye-fill" style="color: primary; font-size:1.5rem;"></i></a></td>
+                        <td class="text-center"><a href="?route&module=edit&idEntreprise=<?= $key['idEntreprise']; ?>"><i class="bi bi-pencil-square" style="color: primary; font-size:1.5rem;"></i></a></td>
+                        <td class="text-center"><a href="?route&module=delete&idEntreprise=<?= $key['idEntreprise']; ?>"><i class="bi bi-trash3" style="color: #ce0033; font-size:1.5rem;"></i></a></td>
                     </tr>
                 <?php } ?>
             </tbody>
